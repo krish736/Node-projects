2433ac476193cd1f2faad39403bff57f79d9a5f5
krish736 <112505529+krish736@users.noreply.github.com>
2024-05-22 22:44:17 +0530

    commit

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -3,6 +3,11 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const connectDB = require("./db/connect");
 
 const authRouter = require("./routes/auth");
@@ -14,8 +19,17 @@ const authenticateUser = require("./middleware/authentication");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet);
+app.use(cors);
+app.use(xss);
 
 // routes
 app.get("/", (req, res) => {
